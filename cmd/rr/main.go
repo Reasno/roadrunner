6c2ecbc83c3a32404ec46d47a136dbb1de8fc395
Reasno <guxi99@gmail.com>
2019-04-29 17:24:51 +0800

    my modification of roadrunner

--- cmd/rr/main.go
+++ cmd/rr/main.go
@@ -23,15 +23,18 @@
 package main
 
 import (
+	"github.com/sirupsen/logrus"
 	rr "github.com/spiral/roadrunner/cmd/rr/cmd"
 
 	// services (plugins)
+	"github.com/spiral/php-grpc"
 	"github.com/spiral/roadrunner/service/env"
 	"github.com/spiral/roadrunner/service/http"
 	"github.com/spiral/roadrunner/service/rpc"
 	"github.com/spiral/roadrunner/service/static"
 
 	// additional commands and debug handlers
+	_ "github.com/spiral/php-grpc/cmd/rr-grpc/grpc"
 	_ "github.com/spiral/roadrunner/cmd/rr/http"
 )
 
@@ -40,6 +43,8 @@ func main() {
 	rr.Container.Register(rpc.ID, &rpc.Service{})
 	rr.Container.Register(http.ID, &http.Service{})
 	rr.Container.Register(static.ID, &static.Service{})
+	rr.Container.Register(grpc.ID, &grpc.Service{})
+	rr.Logger.Formatter = &logrus.TextFormatter{ForceColors: true}
 
 	// you can register additional commands using cmd.CLI
 	rr.Execute()
